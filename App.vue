54d00158bf602edb3f0d31d827b577b994ad0c0a
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-09-20 21:16:54 +0900

    added files

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<p v-if="msg.length > 0">{{ msg }}</p>
+		<p v-else>no text</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			msg: "Hello World!",
+		};
+	},
+};
+</script>
